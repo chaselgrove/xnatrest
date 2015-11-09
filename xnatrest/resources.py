11f685f62971cd04b3f1ec6174f48640670e0e95
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2015-11-09 10:47:57 -0500

    added project resource

--- xnatrest/resources.py
+++ xnatrest/resources.py
@@ -19,8 +19,25 @@ class _ProjectsResource(_Resource):
         url = '/data/archive/projects'
         if params:
             url = '%s?%s' % (url, urllib.urlencode(params))
-        r = self.connection.request('GET', url)
-        print r.status
-        return r.data
+        return self.connection.request('GET', url)
+
+class _ProjectResource(_Resource):
+
+    def __init__(self, connection, project_id):
+        _Resource.__init__(self, connection)
+        self.project_id = project_id
+        return
+
+    def get(self, format=None):
+        params = {}
+        if format is not None:
+            if format not in ('html', 'json', 'xml'):
+                raise ValueError('bad value for format')
+            params['format'] = format
+        url = '/data/archive/projects/%s' % self.project_id
+        if params:
+            url = '%s?%s' % (url, urllib.urlencode(params))
+        res = self.connection.request('GET', url)
+        return res
 
 # eof
