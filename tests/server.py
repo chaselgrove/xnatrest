7086755d750c5b333c899822c32585edd20959d4
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2015-12-08 17:06:14 -0500

    cleaned up

--- tests/server.py
+++ tests/server.py
@@ -25,32 +25,4 @@ class TestServer(unittest.TestCase):
         self.assertEqual(s.version, server_info['version'], server_info['name'])
         return
 
-"""
-    def test_nojsessionid(self):
-        with self.assertRaises(xnatrest.JSESSIONIDError):
-            c = xnatrest.Connection(config.non_xnat_url)
-        return
-
-    def test_bad_auth(self):
-        with self.assertRaises(xnatrest.AuthenticationError):
-            c = xnatrest.Connection('https://central.xnat.org/')
-            c.auth_user_password('nosetests', 'bogus')
-        return
-
-    def test_auth(self):
-        c = xnatrest.Connection('https://central.xnat.org/')
-        self.assertEqual(c.auth_type, 'jsessionid')
-        self.assertIsNone(c.user)
-        c.auth_jsessionid('nosetests', 'nose')
-        self.assertEqual(c.auth_type, 'jsessionid')
-        self.assertEqual(c.user, 'nosetests')
-        c.auth_anonymous()
-        self.assertEqual(c.auth_type, 'jsessionid')
-        self.assertIsNone(c.user)
-        jsessionid = c._jsessionid
-        c.auth_anonymous()
-        self.assertNotEqual(c._jsessionid, jsessionid)
-        return
-"""
-
 # eof
