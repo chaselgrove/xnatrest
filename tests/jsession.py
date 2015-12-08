9f30a184d3cf3f699e66b8993300bd82119fad58
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2015-12-08 17:08:47 -0500

    split jsession get tests

--- tests/jsession.py
+++ tests/jsession.py
@@ -7,11 +7,18 @@ import xnatrest
 class TestJsession(unittest.TestCase):
 
     @config.test_foreach('jsession')
-    def test_get(self, server_info):
+    def test_get_noauth(self, server_info):
         server = xnatrest.Server(server_info['url'])
         resource = xnatrest.JsessionResource(server)
         response = resource.get()
         self.assertEqual(response.status, 200, server_info['name'])
+        return
+
+    @config.test_foreach('jsession')
+    def test_get_token_auth(self, server_info):
+        server = xnatrest.Server(server_info['url'])
+        resource = xnatrest.JsessionResource(server)
+        response = resource.get()
         token = response.data
         headers = {'Cookie': 'JSESSIONID=%s' % token}
         response2 = resource.get(headers=headers)
